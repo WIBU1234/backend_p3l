e3ea84aa6e0a33796ffe10575cca2245bcfb4610
JonathanKelvin02 <210711399@students.uajy.ac.id>
2024-04-02 12:01:23 +0700

    Migration (Add Procedure & Trigger)

---
+++ database/migrations/2024_04_02_114308_insert_tgl_pembayaran.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::unprepared('
+        CREATE TRIGGER insert_tgl_pembayaran
+        BEFORE INSERT ON tblTransaksi
+        FOR EACH ROW
+        BEGIN
+        DECLARE tanggal_wajib_bayar DATETIME;
+        SET tanggal_wajib_bayar = DATE_ADD(NEW.Tanggal_Transaksi, INTERVAL 1 DAY);
+        
+        SET NEW.Tanggal_Pelunasan = tanggal_wajib_bayar;
+        END');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::unprepared('DROP TRIGGER IF EXISTS insert_tgl_pembayaran');
+    }
+};
---
+++ database/migrations/2024_04_02_114319_insert_child_produk.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::unprepared('
+        CREATE TRIGGER insert_child_produk
+        BEFORE INSERT ON tblProduk
+        FOR EACH ROW
+        BEGIN
+        IF (NEW.ID_Produk LIKE "PN%") THEN
+            INSERT INTO tbltitipan VALUES (NEW.ID_Produk, NULL, NULL, NULL);
+        ELSEIF (NEW.ID_Produk LIKE "AK%") THEN
+            INSERT INTO tblresep VALUES (NEW.ID_Produk, NULL);
+        ELSE
+            INSERT INTO tblhampers VALUES (NEW.ID_Produk, NULL);
+        END IF;
+        END');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::unprepared('DROP TRIGGER IF EXISTS insert_child_produk');
+    }
+};
