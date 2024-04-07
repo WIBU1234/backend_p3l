8937dd50e627d9627f336f7423e4a9e3c3372182
Skyesblue <210711019@students.uajy.ac.id>
2024-04-07 13:38:43 +0700

    Fix Middleware Role

--- routes/api.php
+++ routes/api.php
@@ -23,10 +23,24 @@ Route::post('/registerPegawai', [App\Http\Controllers\AuthController::class, 're
 Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
 Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
 
-Route::middleware(['auth:api-pegawai', 'role:Owner'])->group(function () {
+Route::middleware(['auth:api-pegawai', 'role:Admin,Owner,MO'])->group(function () {
+    //Rute yang bisa diakses admin/owner/mo
     Route::post('/logoutPegawai', [App\Http\Controllers\AuthController::class, 'logoutPegawai']);
 });
 
+Route::middleware(['auth:api-pegawai', 'role:Admin'])->group(function () {
+    //Rute yang cuma bisa diakses Admin
+});
+
+Route::middleware(['auth:api-pegawai', 'role:Owner'])->group(function () {
+    //Rute yang cuma bisa diakses Owner
+});
+
+Route::middleware(['auth:api-pegawai', 'role:MO'])->group(function () {
+    //Rute yang cuma bisa diakses MO
+});
+
 Route::middleware(['auth:api-customer', 'role:Customer'])->group(function () {
+    //rute yang cuma bisa diakses customer
     Route::post('/logoutCustomer', [App\Http\Controllers\AuthController::class, 'logoutCustomer']);
 });
