af1b475ad4f27028d0aa5a0aaa05113165c7a9fd
JonathanKelvin02 <210711399@students.uajy.ac.id>
2024-04-09 10:04:11 +0700

    store and update detailResep (rev) [not testing yet]

--- app/Http/Controllers/TbldetailresepController.php
+++ app/Http/Controllers/TbldetailresepController.php
@@ -27,28 +27,26 @@ class TbldetailresepController extends Controller
 
     public function store(array $dataArray) {
         foreach ($dataArray as $data) {
-            $simpanDetailResep = $data;
-        }
-
-        $validate = Validator::make($simpanDetailResep, [
-            'ID_Resep' => 'required',
-            'ID_BahanBaku' => 'required',
-            'Jumlah' => 'required'
-        ]);
-
-        if ($validate->fails()) {
-            return response([
-                'message' => $validate->errors(),
-                'status' => 404
-            ], 404);
-        } else {
-            $tblDetailResep = tbldetailresep::create($simpanDetailResep);
-            return response()->json([
-                'message' => 'Detail Resep Berhasil Disimpan',
-                'status' => 200,
-                'data' => $tblDetailResep
-            ], 200);
-        }
+            $validate = Validator::make($data, [
+                'ID_Resep' => 'required',
+                'ID_BahanBaku' => 'required',
+                'Jumlah' => 'required'
+            ]);
+    
+            if ($validate->fails()) {
+                return response([
+                    'message' => $validate->errors(),
+                    'status' => 404
+                ], 404);
+            } else {
+                $tblDetailResep = tbldetailresep::create($data);
+                return response()->json([
+                    'message' => 'Detail Resep Berhasil Disimpan',
+                    'status' => 200,
+                    'data' => $tblDetailResep
+                ], 200);
+            }
+        }        
     }
 
     public function show(int $id) {
@@ -70,35 +68,33 @@ class TbldetailresepController extends Controller
 
     public function update(array $dataArray, int $id) {
         foreach ($dataArray as $data) {
-            $updateDetailResep = $data;
-        }
-
-        $validate = Validator::make($updateDetailResep, [
-            'ID_Resep' => 'required',
-            'ID_BahanBaku' => 'required',
-            'Jumlah' => 'required'
-        ]);
-
-        if ($validate->fails()) {
-            return response([
-                'message' => $validate->errors(),
-                'status' => 404
-            ], 404);
-        } else {
-            $tblDetailResep = tbldetailresep::find($id);
-
-            if (is_null($tblDetailResep)) {
-                return response()->json([
-                    'message' => 'Detail Resep Tidak Ditemukan',
+            $validate = Validator::make($data, [
+                'ID_Resep' => 'required',
+                'ID_BahanBaku' => 'required',
+                'Jumlah' => 'required'
+            ]);
+    
+            if ($validate->fails()) {
+                return response([
+                    'message' => $validate->errors(),
                     'status' => 404
                 ], 404);
             } else {
-                $tblDetailResep->update($updateDetailResep);
-                return response()->json([
-                    'message' => 'Detail Resep Berhasil Diupdate',
-                    'status' => 200,
-                    'data' => $tblDetailResep
-                ], 200);
+                $tblDetailResep = tbldetailresep::find($id);
+    
+                if (is_null($tblDetailResep)) {
+                    return response()->json([
+                        'message' => 'Detail Resep Tidak Ditemukan',
+                        'status' => 404
+                    ], 404);
+                } else {
+                    $tblDetailResep->update($data);
+                    return response()->json([
+                        'message' => 'Detail Resep Berhasil Diupdate',
+                        'status' => 200,
+                        'data' => $tblDetailResep
+                    ], 200);
+                }
             }
         }
     }
