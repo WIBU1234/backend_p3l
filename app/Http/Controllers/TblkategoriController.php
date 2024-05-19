0d08b2ec18be2b7d6f567c0f50d9692c09265e8f
WIBU1234 <210711186@students.uajy.ac.id>
2024-05-19 17:56:23 +0700

    H

--- app/Http/Controllers/TblkategoriController.php
+++ app/Http/Controllers/TblkategoriController.php
@@ -1,10 +1,32 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\tblkategori;
 use Illuminate\Http\Request;
 
 class TblkategoriController extends Controller
 {
-    //
+    public function getAllKategori(){
+        try{
+            $kategori = tblkategori::all();
+
+            if($kategori->isEmpty()){
+                return response()->json([
+                    'message' => 'Data Kategori Kosong',
+                    'data' => $kategori,
+                ], 200);
+            }
+
+            return response()->json([
+                'message' => 'Fetch All Kategori Success',
+                'data' => $kategori,
+            ], 200);
+
+        } catch(\Exception $e){
+            return response()->json([
+                'message' => 'Fetch All Kategori Failed',
+                'data' => $e->getMessage(),
+            ], 400);
+        }
+    }
 }
